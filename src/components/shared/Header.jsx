0f33c2ab2baf44d9eaf67faf122f5e1f9d86641c
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-25 13:16:23 +0600

    header section start

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+
+import Image from 'next/image'
+import logo from '@/assets/logo.png'
 
 const Header = () => {
-    return (
-        <div>
-            <h2>This is header</h2>
-        </div>
-    );
+  return (
+    <div className='text-center py-8 space-y-4'>
+      <Image className="mx-auto" src={logo} width={300} height={200} alt="Logo" />
+      <p>Journalism With Fear or Favour</p>
+      <p>Date</p>
+      
+    </div>
+  );
 };
 
 export default Header;
