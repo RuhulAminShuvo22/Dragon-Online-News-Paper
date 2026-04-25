649db4d0e55f76eedd12642f30a5e338a6350994
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-26 01:50:44 +0600

    catagories data division related work for middle part

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -3,44 +3,49 @@ import RightSidebar from "@/components/homepage/news/RightSidebar";
 import { div, object } from "framer-motion/client";
 import Image from "next/image";
 
+async function getCategories() {
+  const res = await fetch(
+    "https://openapi.programming-hero.com/api/news/categories",
+  );
 
-async function getCategories () {
-  const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
   const data = await res.json();
   return data.data;
 }
 
+async function getNewsByCategoryId(category_id) {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/category/${category_id}`,
+  );
 
+  const data = await res.json();
+  return data.data;
+}
 
 export default async function Home() {
-
   const categories = await getCategories();
-  console.log(categories.news_category  , "categories")
+  console.log(categories.news_category, "categories");
+
+  const news = await getNewsByCategoryId("03");
+  console.log(news, "news");
 
   return (
     <div className="container mx-auto grid grid-cols-12 gap-4 my-[60px]">
+      <div className=" col-span-3">
+        <LeftSidebar categories={categories} activeId={null}></LeftSidebar>
+      </div>
 
-
-        <div className=" col-span-3">
-
-              <LeftSidebar categories={categories} activeId={null}></LeftSidebar>
-              
+      <div className="font-bold  bg-purple-100 col-span-6">
+        All News
+        <div className="space-y-4">
+          {news.map((n) => {
+            return <div key={n._id} className="p-6 rounded-md border">{n.title}</div>;
+          })}
         </div>
+      </div>
 
-
-        <div className="font-bold text-3xl bg-purple-100 col-span-6">
-              All News
-        </div>
-
-
-        <div className="  col-span-3">
-              <RightSidebar></RightSidebar>
-        </div>
-
-
-        
-
-
+      <div className="  col-span-3">
+        <RightSidebar></RightSidebar>
+      </div>
     </div>
-  )
+  );
 }
