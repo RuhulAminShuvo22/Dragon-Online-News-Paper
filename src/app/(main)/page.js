c9e4cdb1f3d16021ffd545aa664992b72cee34b1
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-25 22:41:28 +0600

    all categories data orginazise

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,7 +1,55 @@
+import { div, object } from "framer-motion/client";
 import Image from "next/image";
 
-export default function Home() {
+
+async function getCategories () {
+  const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+  const data = await res.json();
+  return data.data;
+}
+
+
+
+export default async function Home() {
+
+  const categories = await getCategories();
+  console.log(categories.news_category  , "categories")
+
   return (
-    <h2 className="text-red-500">Home Page</h2>
-  );
+    <div className="container mx-auto grid grid-cols-12 gap-4 my-[60px]">
+
+
+        <div className=" col-span-3">
+
+              <h2 className="font-bold text-lg">
+                All Categories
+              </h2>
+
+            <ul className="flex flex-col gap-3 mt-6">
+             {
+                categories.news_category.map(category => {
+                  return <li key={category.category_id} className="bg-slate-100 p-2 rounded-md font-bold text-center text-md">{category.category_name}</li>
+                })
+              }
+
+            </ul>
+              
+        </div>
+
+
+        <div className="font-bold text-3xl bg-purple-100 col-span-6">
+              All News
+        </div>
+
+
+        <div className="font-bold text-3xl bg-yellow-100 col-span-3">
+              Social Icons
+        </div>
+
+
+        
+
+
+    </div>
+  )
 }
