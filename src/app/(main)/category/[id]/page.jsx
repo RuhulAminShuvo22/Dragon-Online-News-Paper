3d279bf7251c6c6a213317a4fcf4aa174dcbdb87
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-26 13:01:01 +0600

    update on newsCategory page

--- src/app/(main)/category/[id]/page.jsx
+++ src/app/(main)/category/[id]/page.jsx
@@ -1,26 +1,11 @@
 import LeftSidebar from '@/components/homepage/news/LeftSidebar';
 import RightSidebar from '@/components/homepage/news/RightSidebar';
+import { getCategories, getNewsByCategoryId } from '@/lib/data';
 import { h2 } from 'framer-motion/client';
 import React from 'react';
 
 
-async function getCategories() {
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/news/categories",
-  );
 
-  const data = await res.json();
-  return data.data;
-}
-
-async function getNewsByCategoryId(category_id) {
-  const res = await fetch(
-    `https://openapi.programming-hero.com/api/news/category/${category_id}`,
-  );
-
-  const data = await res.json();
-  return data.data;
-}
 
 const NewsCategoryPage = async ({ params }) => {
 
@@ -37,9 +22,9 @@ const news = await getNewsByCategoryId(id)
         <LeftSidebar categories={categories} activeId={id}></LeftSidebar>
       </div>
 
-      <div className="font-bold  bg-purple-100 col-span-6">
-        All News
-        <div className="space-y-4">
+      <div className="font-bold  col-span-6">
+        <h2 className='font-bold text-lg'>News by Category</h2>
+        <div className="space-y-4 mt-6">
           {news.length > 0 ?  news.map((n) => {
             return <div key={n._id} className="p-6 rounded-md border">{n.title}</div>;
           }): <h2 className='font-bold text-4xl text-center my-7'>No news found</h2>}
