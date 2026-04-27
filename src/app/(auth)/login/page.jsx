d126c85e34b8724939a2e375e6aa30a9376397c9
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-27 16:47:26 +0600

    log in page related work on going

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,19 +1,32 @@
+'use client'
+
 import Link from 'next/link';
 import React from 'react';
 
 const LoginPage = () => {
+
+    const handleLoginFunc = (e)=> {
+        e.preventDefault();
+        const email = e.target.email.value ;
+        const password = e.target.password.value ;
+        console.log(email, password)
+    }
+
+
+
     return (
         <div className='container mx-auto min-h-[80vh] flex justify-center items-center bg-slate-100'>
             <div className='p-4 rounded-xl bg-white'>
                 <h2 className='font-bold text-3xl text-center mb-6'>Login your Account</h2>
 
-                <form className='space-y-4'>
+                <form className='space-y-4' onSubmit={handleLoginFunc}>
 
-                    <fieldset className="fieldset">
+                    <fieldset className="fieldset" >
                         <legend className="fieldset-legend">Email Address</legend>
                         <input 
                         type="email" 
                         className="input" 
+                        name='email'
                         placeholder="Type your email" />
                         
                     </fieldset>
@@ -23,6 +36,7 @@ const LoginPage = () => {
                         <input 
                         type="password" 
                         className="input" 
+                        name='password'
                         placeholder="Type your password" />
                         
                     </fieldset>
@@ -31,7 +45,7 @@ const LoginPage = () => {
 
                 </form>
                 <br />
-                <p>Do Not Have An Account ? <Link href={"/register"} className='text-red-500'>Register</Link> </p>
+                <p className='mt-4'>Do Not Have An Account ? <Link href={"/register"} className='text-blue-500'>Register</Link> </p>
             </div>
         </div>
     );
