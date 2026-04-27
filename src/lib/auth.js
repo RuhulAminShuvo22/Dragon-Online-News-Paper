33615139b502eaa77f5b6c221999432e6c09b360
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-28 02:36:40 +0600

    mongodb connection related work

---
+++ src/lib/auth.js
@@ -0,0 +1,29 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+
+const client = new MongoClient(process.env.MONGO_URI);  //mongodb er connection string  l
+const db = client.db();
+
+
+export const auth = betterAuth({
+  database: mongodbAdapter(db, {
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
+  }),
+});
+
+
+
+// import { betterAuth } from "better-auth";
+// import { MongoClient } from "mongodb";
+// import { mongodbAdapter } from "better-auth/adapters/mongodb";
+// const client = new MongoClient("mongodb://localhost:27017/database");
+// const db = client.db();
+// export const auth = betterAuth({
+//   database: mongodbAdapter(db, {
+//     // Optional: if you don't provide a client, database transactions won't be enabled.
+//     client
+//   }),
+// });
