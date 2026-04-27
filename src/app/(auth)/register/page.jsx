65e272e646b859dc9b25c638fe635550a38e020d
Ruhul Amin <ruhul.amin.shuvo.1510@gmail.com>
2026-04-28 01:10:50 +0600

    register page related work

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -15,12 +15,12 @@ const RegisterPage = () => {
 
     const handleRegisterFunc = (data)=> {
         console.log(data,"data")
-        // const {email, name, photo, password} = data;
-        // console.log(name,email,photo,password)
+        const {email, name, photo, password} = data;
+        console.log(name,email,photo,password)
     }
 
-    // console.log(watch("email"))
-    // console.log(watch("password"))
+    console.log(watch("email"))
+    console.log(watch("password"))
 
 
 
